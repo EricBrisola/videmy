d4289cd25679c67e5426d719c60e69a1812c6e60
EricBrisola <eric.coca13@gmail.com>
2025-09-23 02:50:13 -0300

    first commit

---
+++ back/src/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { router } from "./routes/index.ts";
+
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
