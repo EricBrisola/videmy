e630ceb68c20a49fef602de02a0a335b86fb2018
EricBrisola <eric.coca13@gmail.com>
2025-11-23 14:52:43 -0300

    fix: next cache is rendering videos that were removed

--- front/src/utils/getVideos.ts
+++ front/src/utils/getVideos.ts
@@ -2,7 +2,9 @@ import { Video } from "@/types/video";
 
 const getVideos = async (): Promise<Video[] | null> => {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/videos/`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/videos/`, {
+      cache: "no-store",
+    });
 
     if (response.ok) {
       const videos: Video[] = await response.json();
